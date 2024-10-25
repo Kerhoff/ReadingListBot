b2792eccbf24437a8a9a7f1e9cb7397a0703d3f0
Aleksandr Lukashkin <i@kerhoff.com>
2024-10-25 08:20:34 +0200

    Fix delete tests

--- tests/test_handlers/test_delete.py
+++ tests/test_handlers/test_delete.py
@@ -8,38 +8,26 @@ from bot.db import ReadingItem
 
 
 @pytest.mark.parametrize(
-    "args, expected_message, callback_data",
+    "args, expected_message",
     [
-        (
-            ["1"],
-            "Item '1' deleted from your list.",
-            "delete_confirm:1",
-        ),
-        ( ["1"],
-            "Deletion cancelled.",
-            "delete_cancel",
-        ),
-        ([], "Use: /delete <item number>", ""),
-        (["100"], "Invalid item number", ""),
+        (["1"], "Are you sure you want to delete '1. Test Article'?"),
+        ([], "Use: /delete <item number>"),
+        (["100"], "Invalid item number"),
     ],
 )
+
 @pytest.mark.asyncio
-async def test_delete_handler(mocker, args, expected_message, callback_data):
+async def test_delete_handler(mocker, args, expected_message):
     # Mosck the Update and Context objects
     mock_update = mocker.Mock(spec=Update)
     mock_context = mocker.Mock(spec=ContextTypes.DEFAULT_TYPE)
-
-    # Mock the callback_query data
-    mock_query = mocker.Mock()
-    mock_query.data = callback_data
-    mock_update.callback_query = mock_query
 
     # Set up the mock Update
     mock_update.effective_user.id = 171717171
     mock_context.args = args
 
     # Create the confirmation keyboard
-    keyboard = [
+    expected_keyboard = [
         [
             InlineKeyboardButton(
                 "Yes",
@@ -51,16 +39,57 @@ async def test_delete_handler(mocker, args, expected_message, callback_data):
             ),
         ]
     ]
-    reply_markup = InlineKeyboardMarkup(keyboard)
-
-    # Set up the mock Confirmation message
-    confirmation_message = "Are you sure you want to delete '1. Test Article'?"
+    expected_reply_markup = InlineKeyboardMarkup(expected_keyboard)
 
     # Mock the database calls
     mocker.patch(
         "bot.handlers.delete.get_items",
-        return_value=[ReadingItem(id=1, title="Test Article"),],
+        return_value=[
+            ReadingItem(id=1, title="Test Article"),
+            ],
     )
+
+    # Mock the send_message method with AsyncMock
+    mock_context.bot.send_message = AsyncMock()
+
+    # Run the handlers
+    await delete(mock_update, mock_context)
+
+    # Verify the bot's send_message method was called
+    if not args or args[0] == "100":
+        mock_context.bot.send_message.assert_called_once_with(
+            chat_id=mock_update.effective_chat.id,
+            text=expected_message,
+            )
+    else:
+        mock_context.bot.send_message.assert_called_once_with(
+            chat_id=mock_update.effective_chat.id,
+            text=expected_message,
+            reply_markup=expected_reply_markup,)
+
+
+@pytest.mark.parametrize(
+    "callback_data, expected_result_message",
+    [
+        ( 'delete_confirm:1', "Item '1' deleted from your list."),
+        ( "delete_cancel", "Deletion cancelled."),
+    ],
+)
+@pytest.mark.asyncio
+async def test_delete_callback_handler(mocker, callback_data, expected_result_message):
+    # Mosck the Update and Context objects
+    mock_update = mocker.Mock(spec=Update)
+    mock_context = mocker.Mock(spec=ContextTypes.DEFAULT_TYPE)
+
+    # Mock the callback_query data
+    mock_query = mocker.Mock()
+    mock_query.data = callback_data
+    mock_update.callback_query = mock_query
+
+    # Set up the mock Update
+    mock_update.effective_user.id = 171717171
+
+    # Mock the database calls
     mocker.patch("bot.handlers.delete.delete_item", return_value=None)
 
     # Mock the send_message method with AsyncMock
@@ -70,21 +99,9 @@ async def test_delete_handler(mocker, args, expected_message, callback_data):
     mock_query.answer = AsyncMock()
 
     # Run the handlers
-    await delete(mock_update, mock_context)
-
-    # Verify the bot's send_message method was called
-    mock_context.bot.send_message.assert_any_call(
-        chat_id=mock_update.effective_chat.id,
-        text=confirmation_message,
-        reply_markup=reply_markup,
-        )
-    
-    # Run the callback_query handlers
     await delete_callback(mock_update, mock_context)
 
     # Verify the bot's send_message method was called
-    mock_context.bot.send_message.assert_any_call(
-        chat_id=mock_update.effective_chat.id, text=expected_message
+    mock_context.bot.send_message.assert_called_once_with(
+        chat_id=mock_update.effective_chat.id, text=expected_result_message
     )
-
-    assert mock_context.bot.send_message.call_count == 2
